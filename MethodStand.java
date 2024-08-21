b2965e8b8c53d2b833a18f5904ec81f8f9bac9de
ManyaSree05 <154441766+ManyaSree05@users.noreply.github.com>
2024-08-21 23:17:43 +0530

    Add files via upload

---
+++ MethodStand.java
@@ -0,0 +1,9 @@
+public class MethodStand {
+    public static void main(String[] args) {
+        int x = 25;
+        String m = "HelloWorld";
+        System.out.println(Math.sqrt(x));
+        System.out.println(m.toUpperCase());
+        System.out.println(m.toLowerCase());
+    }
+}
